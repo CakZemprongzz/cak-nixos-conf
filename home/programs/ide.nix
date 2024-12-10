bc32af59cfe60b135a905ef78e0ef91b0fe1f864
CakZemprongzz <28718490+CakZemprongzz@users.noreply.github.com>
2024-12-10 13:23:15 +0700

    nani

--- home/programs/ide.nix
+++ home/programs/ide.nix
@@ -7,9 +7,9 @@
   programs = {
     vscode = {
       enable = true;
-      extensions =  with pkgs.vscode-extensions;[
+      #extensions =  with pkgs.vscode-extensions;[
         # {id = "";}  // extension id, query from chrome web store
-      ];
+      #];
     };
   };
 }
