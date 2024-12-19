6efff286029fc3b54064c3dfabfc4bf16f1834fa
CakZemprongzz <28718490+CakZemprongzz@users.noreply.github.com>
2024-12-19 14:49:39 +0700

    updated flakes.nix

--- flake.nix
+++ flake.nix
@@ -1,52 +1,48 @@
 {
-  description = "A simple NixOS flake";
+  description = "A simple NixOS flake with Home Manager";
+
   inputs = {
-      nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
-      home-manager = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    home-manager = {
       url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
-    }; 
- };
+    };
+  };
 
-  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
+  outputs = { self, nixpkgs, home-manager, ... }:
+  let
+    # Define reusable arguments for nixosSystem
+    commonModules = { username, hostFile, userHome }: [
+      hostFile
+      home-manager.nixosModules.home-manager {
+        home-manager.useGlobalPkgs = true;
+        home-manager.useUserPackages = true;
+        home-manager.extraSpecialArgs = { inherit nixpkgs home-manager; };
+        home-manager.users.${username} = import userHome;
+      }
+    ];
+  in
+  {
     nixosConfigurations = {
-      nixos-test = let
-        username = "cak";
-      in
-        nixpkgs.lib.nixosSystem {
-          system = "x86_64-linux";
-          modules = [
-            ./hosts/nixos-test
-            #./users/${username}/nixos.nix
-
-            home-manager.nixosModules.home-manager {
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
-
-              home-manager.extraSpecialArgs = inputs;
-              home-manager.users.mccak= import ./users/cak/home.nix;
-            }
-          ];
+      # Configuration for nixos-test
+      nixos-test = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = commonModules {
+          username = "cak";
+          hostFile = ./hosts/nixos-test;
+          userHome = ./users/cak/home.nix;
         };
+      };
 
-      840-g6 = let
-        username = "cak";
-      in
-        nixpkgs.lib.nixosSystem {
-          system = "x86_64-linux";
-          modules = [
-            ./hosts/840-g6
-            #./users/${username}/nixos.nix
-
-            home-manager.nixosModules.home-manager {
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
-
-              home-manager.extraSpecialArgs = inputs;
-              home-manager.users.mccak= import ./users/cak/home.nix;
-            }
-          ];
+      # Configuration for 840-g6
+      840-g6 = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = commonModules {
+          username = "cak";
+          hostFile = ./hosts/840-g6;
+          userHome = ./users/cak/home.nix;
         };
+      };
     };
   };
 }
