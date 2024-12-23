2bcdf742cd667ee1cdf6fd1a351c4d6a567a2cea
CakZemprongzz <28718490+CakZemprongzz@users.noreply.github.com>
2024-12-23 08:57:57 +0700

    idk

--- modules/system.nix
+++ modules/system.nix
@@ -138,7 +138,18 @@
       dockerCompat = true;
       defaultNetwork.settings.dns_enabled = true;
     };
-    libvirtd.enable = true;
+    libvirtd = {
+      enable = true;
+      qemu = {
+        swtpm.enable = true;
+        ovmf = {
+          enable = true;
+          packages = with pkgs; [
+            OVMFFull.fd
+          ];
+        };
+      };
+    };
     spiceUSBRedirection.enable = true;
   };
 
