f4b33c87f765c8489cc5989e852e95c9e2617027
CakZemprongzz <28718490+CakZemprongzz@users.noreply.github.com>
2025-01-14 05:54:05 +0700

    adding wireguard

--- modules/networking.nix
+++ modules/networking.nix
@@ -4,10 +4,12 @@
     firewall = {
       enable = true;
       allowedTCPPorts = [ 22 ];
+      allowedUDPPorts = [ 51820 ];
       allowedTCPPortRanges = [ { from = 1714; to = 1764; } ];
       allowedUDPPortRanges = [ { from = 1714; to = 1764; } ];
     };
     networkmanager.enable = true;
+    wg-quick.interfaces.wg0.configFile = "/home/cak/wireguard-key/wg0.conf";
   };
 
 }
