338dd196c85ac94eac5d28532a1048c591257e2e
CakZemprongzz <28718490+CakZemprongzz@users.noreply.github.com>
2025-04-05 21:28:46 +0700

    added lix

--- flake.nix
+++ flake.nix
@@ -18,14 +18,20 @@
       url = "github:ezKEa/aagl-gtk-on-nix/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    lix-module = {
+      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.92.0-3.tar.gz";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, nixpkgs-unstable, home-manager, plasma-manager, aagl, ... } @ inputs:
+  outputs = { self, nixpkgs, nixpkgs-unstable, home-manager, plasma-manager, aagl,lix-module, ... } @ inputs:
   let
     makeConfig = { name, username, hostFile, extraModules ? [] }: nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       specialArgs = { inherit inputs; };
       modules = [
+        lix-module.nixosModules.default
         hostFile
         home-manager.nixosModules.home-manager {
           home-manager.useGlobalPkgs = true;
